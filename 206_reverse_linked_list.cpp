a0262e3265c46d87125b54f636de8cb774f93ba8
vaishnavi2833 <vaishnavi.ladda@walchandsangli.ac.in>
2024-07-28 00:34:24 +0530

    day 4

---
+++ 206_reverse_linked_list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* next;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
